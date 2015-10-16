25785a487f416858c20303abce38e6d5a73ce3ec
Leendert Dommicent <dommicent.leendert@gmail.com>
2015-10-16 16:07:24 +0200

    Initial commit

---
+++ OwinTest/Models/IdentityModels.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+
+namespace OwinTest.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IUser<Guid>
+    {
+        public ApplicationUser()
+        {
+            Id = new Guid();
+            FailedAttempts = 0;
+        }
+
+        public string PasswordHash { get; set; }
+
+        public Guid Id { get; private set; }
+        public string UserName { get; set; }
+        public DateTimeOffset LockoutEnd { get; set; }
+        public int FailedAttempts { get; private set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser, Guid> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+        public int IncrementFailedAttepts()
+        {
+            return FailedAttempts++;
+        }
+
+        public void ResetFailedAttempts()
+        {
+            FailedAttempts = 0;
+        }
+    }
+}
---
+++ OwinTest/Stores/UserStore.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using OwinTest.Models;
+
+namespace OwinTest.Stores
+{
+    public class UserStore : IUserPasswordStore<ApplicationUser, Guid>, IUserLockoutStore<ApplicationUser, Guid>, IUserTwoFactorStore<ApplicationUser, Guid>
+    {
+        private static readonly List<ApplicationUser> _users = new List<ApplicationUser>(); 
+
+        public void Dispose()
+        {
+        }
+
+        public Task CreateAsync(ApplicationUser user)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                if (_users.All(x => x.Id != user.Id))
+                {
+                    _users.Add(user);
+                }
+            });
+        }
+
+        public Task UpdateAsync(ApplicationUser user)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                _users.Remove(_users.Single(x => x.Id == user.Id));
+                _users.Add(user);
+            });
+        }
+
+        public Task DeleteAsync(ApplicationUser user)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                _users.Remove(_users.Single(x => x.Id == user.Id));
+            });
+        }
+
+        public Task<ApplicationUser> FindByIdAsync(Guid userId)
+        {
+            return Task.Factory.StartNew(() => _users.SingleOrDefault(x => x.Id == userId));
+        }
+
+        public Task<ApplicationUser> FindByNameAsync(string userName)
+        {
+            return Task.Factory.StartNew(() => _users.SingleOrDefault(x => x.UserName == userName));
+        }
+
+        public Task SetPasswordHashAsync(ApplicationUser user, string passwordHash)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                user.PasswordHash = passwordHash;
+            });
+        }
+
+        public Task<string> GetPasswordHashAsync(ApplicationUser user)
+        {
+            return Task.Factory.StartNew(() => user.PasswordHash);
+        }
+
+        public Task<bool> HasPasswordAsync(ApplicationUser user)
+        {
+            return Task.Factory.StartNew(() => String.IsNullOrEmpty(user.PasswordHash));
+        }
+
+        public Task<DateTimeOffset> GetLockoutEndDateAsync(ApplicationUser user)
+        {
+            return Task.Factory.StartNew(() => user.LockoutEnd);
+        }
+
+        public Task SetLockoutEndDateAsync(ApplicationUser user, DateTimeOffset lockoutEnd)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                user.LockoutEnd = lockoutEnd;
+            });
+        }
+
+        public Task<int> IncrementAccessFailedCountAsync(ApplicationUser user)
+        {
+            return Task.Factory.StartNew(() => user.IncrementFailedAttepts());
+        }
+
+        public Task ResetAccessFailedCountAsync(ApplicationUser user)
+        {
+            return Task.Factory.StartNew(user.ResetFailedAttempts);
+        }
+
+        public Task<int> GetAccessFailedCountAsync(ApplicationUser user)
+        {
+            return Task.Factory.StartNew(() => user.FailedAttempts);
+        }
+
+        public Task<bool> GetLockoutEnabledAsync(ApplicationUser user)
+        {
+            return Task.Factory.StartNew(() => true);
+        }
+
+        public Task SetLockoutEnabledAsync(ApplicationUser user, bool enabled)
+        {
+            return Task.Factory.StartNew(() => {});
+        }
+
+        public Task SetTwoFactorEnabledAsync(ApplicationUser user, bool enabled)
+        {
+            return Task.Factory.StartNew(() => { });
+        }
+
+        public Task<bool> GetTwoFactorEnabledAsync(ApplicationUser user)
+        {
+            return Task.FromResult(false);
+        }
+    }
+}
